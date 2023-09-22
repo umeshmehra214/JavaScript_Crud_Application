246343dff3d2c04bd68d8fa75cd4aea773ca1256
Umesh Mehra <umeshmehra214@gmail.com>
2023-09-22 07:27:23 +0530

    add starting templates

---
+++ script.js
@@ -0,0 +1,24 @@
+var selectedRow = null;
+
+// Show Alert
+function showAlert(message, className){
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector(".alert").remove(), 3000)
+}
+
+
+// Delete Data
+document.querySelector("#student-list").addEventListener("click", (e) =>{
+    target = e.target;
+    if(target.classList.contains("delete")){
+        target.parentElement.parentElement.remove();
+        showAlert("Student Data Deleted", "danger")
+    }
+})
